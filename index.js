f250e5b4fe3f3a4ab3097c3862c120201e9b7c6f
Sparkles2000 <rucker.anna2000@gmail.com>
2021-04-04 22:26:37 -0500

    *updated

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+const drivers=[`Milo`, `Otis`, `Garfield`]
+    function destructivelyAppendDriver() {
+        return drivers.push(`Ralph`)
+    }
+    function destructivelyPrependDriver(Bob){
+        drivers.unshift(`Bob`)
+    }
+    function destructivelyRemoveLastDriver(Ralph){
+        drivers.pop(`Ralph`)
+    }
+    function destructivelyRemoveFirstDriver(Bob){
+        drivers.shift(`Bob`)
+    }
+    function appendDriver(Broom){
+        return [...drivers, `Broom`]
+    }
+    function prependDriver(Arnold){
+        return [`Arnold`, ...drivers]
+    }
+    function removeLastDriver(){
+        let driversCopy = drivers.slice(0, 2)
+        return driversCopy
+    }
+    
+    function removeFirstDriver(){
+        let driversCopy1 = drivers.slice(1, 4)
+        return driversCopy1
+    }
